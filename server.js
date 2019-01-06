318f66d5c1d814c694a503120919446a97e3ddc0
Dennis Lee <dennisholee@gmail.com>
2019-01-07 00:54:49 +0800

    Add audit fix

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var  app = express();
-var  route = express.Route();
+var app = express();
+var route = express.Route();
+var cors = require('cors');
+var myroute = require('./routes/route');
 
 port = process.env.PORT || 3000;
 
-var myroute = require('./routes/route');
+app.use(cors());
 myroute(app);
 
+app.use(cors());
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
